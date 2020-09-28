d464711319c14166f626a4e6efce6c6ffda47fe0
Christian Ray Leovido <christian.leovido@gmail.com>
2020-09-28 12:18:30 +0100

    add ExampleMovieCell for composable view

--- MovieSearch/View/MovieCell.swift
+++ MovieSearch/View/MovieCell.swift
@@ -9,14 +9,52 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-  
+
 
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var poster: UIImageView!
   @IBOutlet weak var year: UILabel!
-  
+
   override func awakeFromNib() {
       super.awakeFromNib()
   }
-  
+
+}
+
+class ExampleMovieCell: UITableViewCell {
+
+	let titleLabel: UILabel = {
+
+		let label = UILabel()
+
+		// customize your label here..
+		label.backgroundColor = .red
+
+		return label
+
+	}()
+
+	let poster: UIImageView = {
+		return UIImageView()
+	}()
+
+	let year: UILabel = {
+		return UILabel()
+	}()
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+
+		let stackView = UIStackView(arrangedSubviews: [titleLabel,
+													   poster,
+													   year])
+
+		stackView.axis = .vertical
+		stackView.spacing = 5
+		stackView.distribution = .fillEqually
+
+		addSubview(stackView)
+
+	}
+
 }
